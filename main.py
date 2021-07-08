c3713a2677773965a0c0eb6fdc31ac2eee69056a
Drained0001 <87109560+Drained0001@users.noreply.github.com>
2021-07-07 23:26:00 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+import discord
+from discord.ext import commands
+import random
+import os
+from colorama import Fore, init
+from os import system
+
+client = commands.Bot(command_prefix="dm!", self_bot=True)
+
+client.load_extension(f'cogs.auto_fish')
+client.load_extension(f'cogs.auto_hunt')
+
+@client.command()
+async def unload(ctx, extension):
+    if ctx.author.id == client.user.id:
+        client.unload_extension(f'cogs.{extension}')
+
+@client.command()
+async def load(ctx, extension):
+    if ctx.author.id == client.user.id:
+        client.load_extension(f'cogs.{extension}')
+
+@client.command()
+async def reload(ctx, extension):
+    if ctx.author.id == client.user.id:
+        client.unload_extension(f'cogs.{extension}')
+        client.load_extension(f'cogs.{extension}')
+
+@client.command()
+async def quit(ctx):
+    exit()
+
+@client.command()
+async def show_inv(ctx, amt:int):
+    await ctx.message.delete()
+    for a in range(amt):
+        msg = await ctx.send(f"pls inv {a+1}")
+        await msg.delete()
+
+client.run("discord account token", bot=False)
